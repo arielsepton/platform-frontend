8a3d5a6852f7ecdcf201d9693ba2bffd035f3c2b
arielsepton <arielsepton1@gmail.com>
2024-06-05 14:47:33 +0300

    add vite config
    Signed-off-by: arielsepton <arielsepton1@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), svgr(), TanStackRouterVite(), tsconfigPaths()],
   resolve: {
     alias: {
-      src: "/src",
-      components: "/src/Components",
+      "/@": path.resolve(__dirname, "./src/"),
     },
   },
 });
