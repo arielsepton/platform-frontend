b442e13994a86094fa7b7ad70cb2ef1f5180f72f
arielsepton <arielsepton1@gmail.com>
2024-06-05 14:50:31 +0300

    fix ci
    Signed-off-by: arielsepton <arielsepton1@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 export default defineConfig({
   plugins: [react(), svgr(), TanStackRouterVite(), tsconfigPaths()],
   resolve: {
     alias: {
-      "/@": path.resolve(__dirname, "./src/"),
+      "@/": "/src",
+      components: "/src/Components",
     },
   },
 });
