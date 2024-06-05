7bceea6ae04276ebdf24cb1dd5fcbb3814439e37
arielsepton <arielsepton1@gmail.com>
2024-06-05 14:54:25 +0300

    run prettier
    Signed-off-by: arielsepton <arielsepton1@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 export default defineConfig({
   plugins: [react(), svgr(), TanStackRouterVite(), tsconfigPaths()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: {
+      src: "/src",
+    },
   },
 });
